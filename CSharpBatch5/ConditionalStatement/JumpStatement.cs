e70da09e1bd2c460173a5b040bd3564a2c2d9672
Jay Prakash OSFIGO <jayprakashutw@outlook.com>
2025-08-12 10:20:59 +0530

    codepush

---
+++ CSharpBatch5/ConditionalStatement/JumpStatement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBatch5.ConditionalStatement
+{
+    internal class JumpStatement
+    {
+        public void NaturalNo()
+        {
+            int i= 1;
+
+      Nat:
+
+            Console.WriteLine(i);
+            i++;
+            if (i <= 10)
+                goto Nat;
+        }
+        public void EvenNo()
+        {
+            int i = 2;
+
+        Nat:
+
+            Console.WriteLine(i);
+            i=i+2;
+            if (i <= 20)
+                goto Nat;
+        }
+    }
+}
---
+++ CSharpBatch5/ConditionalStatement/SwitchCaseExample.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBatch5.ConditionalStatement
+{
+    internal class SwitchCaseExample
+    {
+        int age, num1, num2, res, choice;
+        char ch;
+
+        public void SimpleCalculator()
+        {
+            Console.WriteLine("Enter 2 nos");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your choice \n1. For Addition \n2. For Subtraction \n3. For Multiplication \n4. For Division");
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            switch(choice)
+            {
+                case 1: 
+                          res = num1 + num2;
+                         Console.WriteLine("Sum of two number is "+res);
+                         break;
+                case 2: 
+                        res = num1 - num2;
+                         Console.WriteLine("Subtraction is " + res);
+                         break;
+                case 3:
+                        res = num1 * num2;
+                        Console.WriteLine("Multiplication is " + res);
+                        break;
+                case 4:
+                        res = num1 / num2;
+                        Console.WriteLine("Division is " + res);
+                        break;
+                default:
+                        Console.WriteLine("Wrong Choice");
+                        break;
+            }
+        }
+        public void SimpleCalculator1()
+        {
+            Console.WriteLine("Enter 2 nos");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your choice \n+. For Addition \n-. For Subtraction \n*. For Multiplication \n/. For Division");
+            ch = Convert.ToChar(Console.ReadLine());
+
+            switch (ch)
+            {
+                case '+':
+                    res = num1 + num2;
+                    Console.WriteLine("Sum of two number is " + res);
+                    break;
+                case '-':
+                    res = num1 - num2;
+                    Console.WriteLine("Subtraction is " + res);
+                    break;
+                case '*':
+                    res = num1 * num2;
+                    Console.WriteLine("Multiplication is " + res);
+                    break;
+                case '/':
+                    res = num1 / num2;
+                    Console.WriteLine("Division is " + res);
+                    break;
+                default:
+                    Console.WriteLine("Wrong Choice");
+                    break;
+            }
+        }
+    }
+}
