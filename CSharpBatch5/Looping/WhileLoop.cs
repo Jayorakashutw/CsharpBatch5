06cf44ac1d894f184106c72bba8645a529128c95
Jay Prakash OSFIGO <jayprakashutw@outlook.com>
2025-09-03 10:12:16 +0530

    Till Single Array All COde Restore

--- CSharpBatch5/Looping/WhileLoop.cs
+++ CSharpBatch5/Looping/WhileLoop.cs
@@ -18,5 +18,19 @@ namespace CSharpBatch5.Looping
                 i++; // increment  so that i can increase from 1 to 5
             }
         }
+
+        public void ReverseNumber()
+        {
+            Console.WriteLine("Enter a number ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int reverse = 0;
+            while (num > 0)
+            {
+                int digit = num % 10;
+                reverse = reverse * 10 + digit;
+                num = num / 10;
+            }
+            Console.WriteLine("Now the reverse number is " + reverse);
+        }
     }
 }
