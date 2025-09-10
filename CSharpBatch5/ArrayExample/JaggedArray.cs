dc0e4da766ee304ae86bcd507f9bb3db9ec21f4b
Jay Prakash OSFIGO <jayprakashutw@outlook.com>
2025-09-10 10:01:45 +0530

    From Array to String and Math Functions

---
+++ CSharpBatch5/ArrayExample/JaggedArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBatch5.ArrayExample
+{
+    internal class JaggedArray
+    {
+        public void JaggedArrayExample()
+        {
+            int[][] jarray = new int[3][];
+            jarray[0] = new int[] { 1, 3, 5, 7, 8 };
+            jarray[1] = new int[] {11, 13 };
+            jarray[2] = new int[] { 25,36,25,365,14,25,365,25,32,356,42 };
+
+            Console.WriteLine("Stored value of Jagged Array is Below");
+            for (int i = 0; i < jarray.Length; i++)
+            { 
+              Console.WriteLine("Rows"+ (i+1) +" : ");
+               for(int j=0; j < jarray[i].Length; j++)
+                {
+                    Console.Write(jarray[i][j]+"\t");
+                }
+                Console.WriteLine();
+                
+            }
+        }
+    }
+}
---
+++ CSharpBatch5/Functions/Math_String.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBatch5.Functions
+{
+    internal class Math_String
+    {
+        public void StringFunctions()
+        {
+            // to find the lenght of a string
+            string s = "Jay prakash pathak";
+            int l = s.Length;
+            Console.WriteLine("Lenght of String is " + l);
+            // to print each character of string
+            foreach (char c in s)
+            {
+                Console.Write(c + " ");
+            }
+
+            // to compare two string
+            string s1 = "Jay Prakash";
+            string s2 = "jay prakash";
+            int res = String.Compare(s1, s2);
+            if (s1.CompareTo(s2) == 0)
+            {
+                Console.WriteLine("Both String are equal");
+            }
+            else
+            {
+                Console.WriteLine("String are not equal");
+            }
+            string[] s3 = { "Jay", "Prakash", "Pathak" };
+            Console.WriteLine(s1.EndsWith("Prakash"));
+            String result = String.Join(" , ", s3);
+            Console.WriteLine("New String is " + result);
+
+            Console.WriteLine("\n\nString in lower case   " + s1.ToLower());
+            Console.WriteLine("String in lower case  " + s1.ToUpper());
+
+        }
+        public void MathFunction()
+        {
+            Console.WriteLine(Math.Max(25,54));//Maximum
+            Console.WriteLine(Math.Min(25, 54));//minimum
+            Console.WriteLine(Math.Sqrt(25));//square root
+            Console.WriteLine(Math.Abs(-85)); //absolute - return absolute positive number
+            Console.WriteLine(Math.Round(25.54)); // round to the nearest whole number
+            Console.WriteLine(Math.Pow(25, 2)); //power
+            Console.WriteLine(Math.Round(Math.PI,2)); //return PI value
+        }
+    }
+}
